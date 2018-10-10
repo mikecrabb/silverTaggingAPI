f27525e3edc03713f5f8953450651be0eb3f4a77
Michael Crabb <mike@crabb.info>
2018-10-10 19:41:00 +0100

    updated tagged methods view (added guidelineID

--- inc/api/apiTaggedMethods.php
+++ inc/api/apiTaggedMethods.php
@@ -3,7 +3,7 @@ include("scripts/dbconnect.php");
 
 $categoryID = $params['categoryID'];
 
-$sql = "SELECT methodID, uniqueName, shortDesc, longDesc, example, test FROM taggedmethodsview where tagID ='" . $categoryID . "'";
+$sql = "SELECT methodID, uniqueName, shortDesc, longDesc, example, test, guidelineID FROM taggedmethodsview where tagID ='" . $categoryID . "'";
 $rows = array();
 $result = $db->query($sql);
 while ($row = $result->fetch_assoc()) {
